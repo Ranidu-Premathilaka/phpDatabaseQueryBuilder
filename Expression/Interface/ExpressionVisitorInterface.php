ceed04c3fc5643aaf5784ced90f4532cb94c2c69
Ranidu-Premathilaka <ranidulakshan001@gmail.com>
2026-01-11 00:07:04 +0530

    bugfix: modified the ExpressionVisitorInterface to reflect new Expressions

--- Expression/Interface/ExpressionVisitorInterface.php
+++ Expression/Interface/ExpressionVisitorInterface.php
@@ -4,5 +4,8 @@ Interface ExpressionVisitorInterface{
     public function visitColumn(Column $column);
     public function visitFunctionCall(FunctionCall $functionCall);
     public function visitLiteral(Literal $literal);
+    public function visitAsc(Asc $asc);
+    public function visitDesc(Desc $desc);
+    public function visitTable(Table $table);
 
 }
